d3e3ce2c85d139a36efc14c3b672dc277a7e8096
mu11ern <mu11ern@yahoo.com>
2023-04-07 16:29:48 -0700

    successfully checking for palindrome

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,19 @@
 const elem = document.querySelector('input');
-
+let log = document.getElementById('values');
 elem.addEventListener('input', handleInput);
+
+function handleInput(input) {
+  inputString = input.target.value;
+  if (inputString.length > 0) {
+    log.textContent = isPalindrome(inputString);
+  }
+}
+
+function isPalindrome(inputString) {
+  for (let i = 0; i < inputString.length / 2; i++) {
+    if (inputString[i] !== inputString[inputString.length - 1 - i]) {
+      return 'No. Try again.';
+    }
+  }
+  return 'Yes. This is a palindrome!';
+}
