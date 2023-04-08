20b0f0cff1adef074ca7bc8147d5d43b26b43eba
mu11ern <mu11ern@yahoo.com>
2023-04-07 17:30:28 -0700

    isPalindrome with colored response

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,19 +1,27 @@
 const elem = document.querySelector('input');
-let log = document.getElementById('values');
+let logNegative = document.getElementById('notPalindrome');
+let logPositive = document.getElementById('isPalindrome');
+
 elem.addEventListener('input', handleInput);
 
 function handleInput(input) {
   inputString = input.target.value;
   if (inputString.length > 0) {
-    log.textContent = isPalindrome(inputString);
+    if (isPalindrome(inputString)) {
+      logPositive.textContent = 'Yes. This is a Palindrome';
+      logNegative.textContent = null;
+    } else {
+      logNegative.textContent = 'No. Try Again.';
+      logPositive.textContent = null;
+    }
   }
 }
 
 function isPalindrome(inputString) {
   for (let i = 0; i < inputString.length / 2; i++) {
     if (inputString[i] !== inputString[inputString.length - 1 - i]) {
-      return 'No. Try again.';
+      return false;
     }
   }
-  return 'Yes. This is a palindrome!';
+  return true;
 }
