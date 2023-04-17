4dc42435707daf7a4f09ff523bbf33f160a44a9e
Nicholas Muller <nmuller@pdx.edu>
2023-04-16 17:36:42 -0700

    work on highlight

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,42 @@
-// Add your code here
+// // Add your code here
+// const elem = document.querySelector('input');
 
-input.addEventListener('keydown', handleKeyDown);
+
+// handleKeyDown = function inputForStringSearch(){
+//     let search = document.getElementById("search").value.trim();
+//     if(search !== ""){
+//         console.log(search)
+//         let warAndPeace = document.getElementById("textBlock").innerHTML;
+//         let regex = new RegExp(search, 'g')
+//         let newText = warAndPeace.replace(regex, `<mark>${search}</mark>`)
+//         document.getElementById("textBlock").innerHTML = newText;
+//     }
+// }
+
+// elem.addEventListener('keydown', handleKeyDown);
+
+const $box = document.getElementById('textBlock');
+
+const $search = document.getElementById('search');
+const originalText =  document.getElementById('textBlock').innerText
+
+$search.addEventListener('input', (event) => {
+        const searchText = event.target.value;
+    if(searchText === ""){
+        $box.innerHTML = originalText;
+    }
+     
+    else{
+        const regex = new RegExp(searchText, 'gi');
+
+        let text = $box.innerHTML;
+
+        text = text.replace(/(<mark class="highlight">|<\/mark>)/gim, '');
+
+        const newText = text.replace(regex, '<mark class="highlight">$&</mark>');
+
+        $box.innerHTML = newText;
+    }
+    
+
+});
