42d840628addd91f3c34fa9a22282ffea1e799ef
Nicholas Muller <nmuller@pdx.edu>
2023-04-17 21:38:37 -0700

    highlight needs style. started background

--- hw2/04-background.js
+++ hw2/04-background.js
@@ -1,1 +1,13 @@
+const interval = document.getElementById('number');
+const submit = document.getElementById('submit');
+const background = document.getElementById('background');
 // Add your code here
+
+submit.addEventListener('click', (event) => {
+    setInterval(changeColor, interval*1000);
+})
+
+changeColor = function changeBackgroundColorEveryInterval() {
+    background.innerHTML
+    
+}
