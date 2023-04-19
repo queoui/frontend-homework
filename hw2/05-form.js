4212b3243422530a629d3fe2b7ad1aabc40fcf03
Nicholas Muller <nmuller@pdx.edu>
2023-04-18 21:58:07 -0700

    05

--- hw2/05-form.js
+++ hw2/05-form.js
@@ -1,1 +1,15 @@
 // Add your code here
+const userName = document.getElementById('InputName').value
+const submit = document.getElementById('Submit');
+const reset = document.getElementById('Reset');
+
+
+console.log(userName)
+
+submit.addEventListener('click', ()=>{
+
+})
+
+reset.addEventListener('click', ()=>{
+    
+})
