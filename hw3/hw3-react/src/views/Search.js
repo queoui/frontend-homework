60889f706a0c1d1b6be5712c7def5761516be6c1
mu11ern <mu11ern@yahoo.com>
2023-05-06 22:06:07 -0700

    end of day

--- hw3/hw3-react/src/views/Search.js
+++ hw3/hw3-react/src/views/Search.js
@@ -1,32 +1,12 @@
 import React, { useState, useEffect } from 'react';
-// import { dataAPI } from '../utils/callAPI';
 
 function Search(props) {
   const [post, setPost] = useState('');
   const [data, setData] = useState([]);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [toggle, setToggle] = useState(false);
   const { title } = props;
-  const [name, setName] = useState('');
-  const [pic, setPic] = useState('');
-
-  const handleChange = (event) => {
-    setPost(event.target.value);
-  };
-
-  const handleClick = () => {
-    data.forEach((character) => {
-      if (post === character.fullName) {
-        setToggle(true);
-        setName(character.fullName);
-        setPic(character.imageUrl);
-        console.log('match');
-      } else {
-        setToggle(false);
-        setName('');
-      }
-    });
-  };
+  let [name, setName] = useState('');
+  let [pic, setPic] = useState('');
 
   const url = 'https://thronesapi.com/api/v2/Characters';
   useEffect(() => {
@@ -37,7 +17,30 @@ function Search(props) {
         setIsLoaded(true);
       })
       .catch((error) => console.log(error));
-  }, [url]);
+  }, [data]);
+
+  const handleChange = (event) => {
+    setPost(event.target.value);
+  };
+
+  const handleClick = (nameChange, picChange) => {
+    console.log(post);
+    console.log(name);
+    data.forEach((character) => {
+      if (post === character.fullName) {
+        console.log('match');
+        nameChange = character.fullName;
+        picChange = character.imageUrl;
+      }
+    });
+    console.log(post);
+    console.log({ name });
+
+    if (post !== name) {
+      setName('');
+      setPic('');
+    }
+  };
 
   return (
     <main className="container">
@@ -55,13 +58,14 @@ function Search(props) {
         <button
           type="button"
           className="btn btn-outline-primary"
-          onClick={handleClick}
+          onClick={handleClick(name, pic)}
         >
           search
         </button>
       </div>
 
       {!isLoaded && <div>Loading...</div>}
+
       <h1>{name}</h1>
       <img src={pic} width="225" height="225"></img>
     </main>
